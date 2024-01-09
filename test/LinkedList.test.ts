bc4dffde66f710bfcf0e18ac517b3b34e8eae41d
YJS <y.j3ms.n@gmail.com>
2024-01-10 08:03:01 +0900

    test(LinkedList): add tests for take

--- test/LinkedList.test.ts
+++ test/LinkedList.test.ts
@@ -19,9 +19,75 @@ describe("range:", () => {
     expect(LL.unsafeToArray(LL.range(toThunk(3), toThunk(7)))).toEqual([3, 4, 5, 6]);
   });
 });
-
 describe("$range:", () => {
-  it("should generate a properly bounded lazy list", () => {
+  it("should generate a properly bounded `[)` lazy list", () => {
     expect(LL.unsafeToArray(LL.$range(3, 7))).toEqual([3, 4, 5, 6]);
+  });
+});
+
+describe("take", () => {
+  it("should take exact amount of the given n", () => {
+    const xs = LL.fromArray("Hello, World!".split(""));
+    const subXs = LL.take(toThunk(4), xs);
+    const result = "Hell".split("");
+    expect(LL.unsafeToArray(subXs)).toEqual(result);
+  });
+  it("should return empty list if the given list is empty", () => {
+    const xs = LL.fromArray([]);
+    const subXs = LL.take(toThunk(100), xs);
+    expect(LL.unsafeToArray(subXs)).toEqual([]);
+  });
+  it("should not return more than the length of list", () => {
+    const xs = LL.fromArray([0, 1, 2, 3]);
+    const subXs = LL.take(toThunk(100), xs);
+    const result = [0, 1, 2, 3];
+    expect(LL.unsafeToArray(subXs)).toEqual(result);
+  });
+  it("should work with an infinite list", () => {
+    const xs = LL.range(toThunk(0));
+    const subXs = LL.take(toThunk(4), xs);
+    const result = [0, 1, 2, 3];
+    expect(LL.unsafeToArray(subXs)).toEqual(result);
+  });
+  it("should take nothing when the given n is less than 1", () => {
+    const xs = LL.range(toThunk(0));
+    let subXs = LL.take(toThunk(0), xs);
+    expect(LL.unsafeToArray(subXs)).toEqual([]);
+
+    subXs = LL.take(toThunk(-1), xs);
+    expect(LL.unsafeToArray(subXs)).toEqual([]);
+  });
+});
+describe("$take", () => {
+  it("should take exact amount of the given n", () => {
+    const xs = LL.fromArray("Hello, World!".split(""));
+    const subXs = LL.$take(4, xs);
+    const result = "Hell".split("");
+    expect(LL.unsafeToArray(subXs)).toEqual(result);
+  });
+  it("should return empty list if the given list is empty", () => {
+    const xs = LL.fromArray([]);
+    const subXs = LL.$take(100, xs);
+    expect(LL.unsafeToArray(subXs)).toEqual([]);
+  });
+  it("should not return more than the length of list", () => {
+    const xs = LL.fromArray([0, 1, 2, 3]);
+    const subXs = LL.$take(100, xs);
+    const result = [0, 1, 2, 3];
+    expect(LL.unsafeToArray(subXs)).toEqual(result);
+  });
+  it("should work with an infinite list", () => {
+    const xs = LL.range(toThunk(0));
+    const subXs = LL.$take(4, xs);
+    const result = [0, 1, 2, 3];
+    expect(LL.unsafeToArray(subXs)).toEqual(result);
+  });
+  it("should take nothing when the given n is less than 1", () => {
+    const xs = LL.range(toThunk(0));
+    let subXs = LL.$take(0, xs);
+    expect(LL.unsafeToArray(subXs)).toEqual([]);
+
+    subXs = LL.$take(-1, xs);
+    expect(LL.unsafeToArray(subXs)).toEqual([]);
   });
 });
