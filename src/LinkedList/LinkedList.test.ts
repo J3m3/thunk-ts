542304b2dbaf7fe8b067baae1522531dc155c73b
YJS <y.j3ms.n@gmail.com>
2024-01-14 22:08:28 +0900

    test: update tests to align with LazyList's type constraint update

--- src/LinkedList/LinkedList.test.ts
+++ src/LinkedList/LinkedList.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "@jest/globals";
-import { toThunk } from "../Thunk";
+import { Thunk, toThunk } from "../Thunk";
 import * as LL from ".";
 
 const STACK_OVERFLOW_BOUND = 10000000;
@@ -19,117 +19,75 @@ describe("fromArray:", () => {
     expect(LL.fromArray([])()).toEqual(null);
   });
   it("should maintain consistency when converting from array to linked list and back", () => {
-    expect(LL.unsafeToArray(LL.fromArray<number>([1, 2, 3]))).toEqual([1, 2, 3]);
+    expect(LL.unsafeToArray(LL.fromArray([1, 2, 3]))).toEqual([1, 2, 3]);
   });
 });
 
 describe("range:", () => {
   it("should generate a properly bounded `[)` lazy list", () => {
-    const xs = LL.range(toThunk(3), toThunk(7));
+    const xs = LL.range(3, 7);
     const result = [3, 4, 5, 6];
     expect(LL.unsafeToArray(xs)).toEqual(result);
   });
   it("should return empty list if start >= end", () => {
-    const xs = LL.range(toThunk(7), toThunk(3));
-    expect(LL.unsafeToArray(xs)).toEqual([]);
-  });
-});
-describe("$range:", () => {
-  it("should generate a properly bounded `[)` lazy list", () => {
-    const xs = LL.$range(3, 7);
-    const result = [3, 4, 5, 6];
-    expect(LL.unsafeToArray(xs)).toEqual(result);
-  });
-  it("should return empty list if start >= end", () => {
-    const xs = LL.$range(7, 3);
+    const xs = LL.range(7, 3);
     expect(LL.unsafeToArray(xs)).toEqual([]);
   });
 });
 
 describe("take:", () => {
   it("should take exact amount of the given n", () => {
-    const xs = LL.fromArray<string>("Hello, World!".split(""));
-    const subXs = LL.take(toThunk(4), xs);
+    const xs = LL.fromArray("Hello, World!".split(""));
+    const subXs = LL.take(4, xs);
     const result = "Hell".split("");
     expect(LL.unsafeToArray(subXs)).toEqual(result);
   });
   it("should return empty list if the given list is empty", () => {
     const xs = LL.fromArray([]);
-    const subXs = LL.take(toThunk(100), xs);
+    const subXs = LL.take(100, xs);
     expect(LL.unsafeToArray(subXs)).toEqual([]);
   });
   it("should not return more than the length of list", () => {
-    const xs = LL.fromArray<number>([0, 1, 2, 3]);
-    const subXs = LL.take(toThunk(100), xs);
+    const xs = LL.fromArray([0, 1, 2, 3]);
+    const subXs = LL.take(100, xs);
     const result = [0, 1, 2, 3];
     expect(LL.unsafeToArray(subXs)).toEqual(result);
   });
   it("should work with an infinite list", () => {
-    const xs = LL.range(toThunk(0));
-    const subXs = LL.take(toThunk(4), xs);
+    const xs = LL.range(0);
+    const subXs = LL.take(4, xs);
     const result = [0, 1, 2, 3];
     expect(LL.unsafeToArray(subXs)).toEqual(result);
   });
   it("should take nothing when the given n is less than 1", () => {
-    const xs = LL.range(toThunk(0));
-    let subXs = LL.take(toThunk(0), xs);
+    const xs = LL.range(0);
+    let subXs = LL.take(0, xs);
     expect(LL.unsafeToArray(subXs)).toEqual([]);
 
-    subXs = LL.take(toThunk(-1), xs);
-    expect(LL.unsafeToArray(subXs)).toEqual([]);
-  });
-});
-describe("$take:", () => {
-  it("should take exact amount of the given n", () => {
-    const xs = LL.fromArray<string>("Hello, World!".split(""));
-    const subXs = LL.$take(4, xs);
-    const result = "Hell".split("");
-    expect(LL.unsafeToArray(subXs)).toEqual(result);
-  });
-  it("should return empty list if the given list is empty", () => {
-    const xs = LL.fromArray([]);
-    const subXs = LL.$take(100, xs);
-    expect(LL.unsafeToArray(subXs)).toEqual([]);
-  });
-  it("should not return more than the length of list", () => {
-    const xs = LL.fromArray<number>([0, 1, 2, 3]);
-    const subXs = LL.$take(100, xs);
-    const result = [0, 1, 2, 3];
-    expect(LL.unsafeToArray(subXs)).toEqual(result);
-  });
-  it("should work with an infinite list", () => {
-    const xs = LL.range(toThunk(0));
-    const subXs = LL.$take(4, xs);
-    const result = [0, 1, 2, 3];
-    expect(LL.unsafeToArray(subXs)).toEqual(result);
-  });
-  it("should take nothing when the given n is less than 1", () => {
-    const xs = LL.range(toThunk(0));
-    let subXs = LL.$take(0, xs);
-    expect(LL.unsafeToArray(subXs)).toEqual([]);
-
-    subXs = LL.$take(-1, xs);
+    subXs = LL.take(-1, xs);
     expect(LL.unsafeToArray(subXs)).toEqual([]);
   });
 });
 
-describe("$filter:", () => {
+describe("filter:", () => {
   it("should properly filter elements by predicate", () => {
     const isEven = (e: number) => e % 2 === 0;
+    const isEvenT = (e: Thunk<number>) => e() % 2 === 0;
     const length = 7;
-    const xs = LL.$range(0, length);
+    const xs = LL.range(0, length);
     const result = [0, 1, 2, 3, 4, 5, 6].filter(isEven);
-    expect(LL.unsafeToArray(LL.$filter(isEven, xs))).toEqual(result);
+    expect(LL.unsafeToArray(LL.filter(isEvenT, xs))).toEqual(result);
   });
   it("should return an empty list if all the elements violates the predicate", () => {
-    const isSpace = (e: string) => e === " ";
-    const xs = LL.$filter(isSpace, LL.fromArray<string>("Hello!".split("")));
+    const isSpaceT = (e: Thunk<string>) => e() === " ";
+    const xs = LL.filter(isSpaceT, LL.fromArray("Hello!".split("")));
     expect(LL.unsafeToArray(xs)).toEqual([]);
   });
   it("should work with an infinite list", () => {
     const isOdd = (e: number) => e % 2 === 1;
+    const isOddT = (e: Thunk<number>) => e() % 2 === 1;
     const length = 5;
-    const xs = LL.$take(length, LL.$filter(isOdd, LL.$range(0)));
+    const xs = LL.take(length, LL.filter(isOddT, LL.range(0)));
     const result = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].filter(isOdd);
     expect(LL.unsafeToArray(xs)).toEqual(result);
   });
@@ -139,29 +97,30 @@ describe("$filter:", () => {
       throw new Error("ERROR!");
     };
     const test = () => {
-      return LL.$filter(generateErr, LL.$range(0, 10));
+      return LL.filter(generateErr, LL.range(0, 10));
     };
     expect(test).not.toThrow();
   });
 });
 
-describe("$map:", () => {
+describe("map:", () => {
   it("should properly map elements by the given function", () => {
     const square = (e: number) => e * e;
+    const squareT = (e: Thunk<number>) => toThunk(e() * e());
     const length = 7;
-    const xs = LL.$map(square, LL.$range(0, length));
+    const xs = LL.map(squareT, LL.range(0, length));
     const result = range(0, length).map(square);
     expect(LL.unsafeToArray(xs)).toEqual(result);
   });
   it("should return an empty list if the given list is empty", () => {
-    const f = <T>(_: T) => (void _, undefined);
-    const xs = LL.$map(f, LL.fromArray([]));
+    const xs = LL.map((_) => toThunk((void _, undefined)), LL.fromArray([]));
     expect(LL.unsafeToArray(xs)).toEqual([]);
   });
   it("should work with an infinite list", () => {
     const f = <T>(_: T) => (void _, "HACKED");
+    const fT = <T>(_: T) => toThunk((void _, "HACKED"));
     const length = 10;
-    const xs = LL.$take(length, LL.$map(f, LL.$range(0)));
+    const xs = LL.take(length, LL.map(fT, LL.range(0)));
     const result = range(0, length).map(f);
     expect(LL.unsafeToArray(xs)).toEqual(result);
   });
@@ -171,65 +130,66 @@ describe("$map:", () => {
       throw new Error("ERROR!");
     };
     const test = () => {
-      return LL.$map(generateErr, LL.$range(0, 10));
+      return LL.map(generateErr, LL.range(0, 10));
     };
     expect(test).not.toThrow();
   });
 });
 
-describe("$fold:", () => {
+describe("fold:", () => {
   it("should properly fold elements by the given function", () => {
     const f = (acc: number, x: number) => x + acc;
+    const fT = (acc: Thunk<number>, x: Thunk<number>) => toThunk(x() + acc());
     const length = 10;
-    const xs = LL.$range(0, length);
+    const xs = LL.range(0, length);
     const result = range(0, length).reduce(f, 0);
-    expect(LL.$fold(f, 0, xs)).toEqual(result);
+    expect(LL.fold(fT, toThunk(0), xs)()).toEqual(result);
   });
   it("should return the given initial value if the given list is empty", () => {
-    const f = (acc: string, x: string) => `${acc} with ${x}`;
+    const fT = (acc: Thunk<string>, x: Thunk<string>) => toThunk(`${acc()} with ${x()}`);
     const xs = LL.fromArray([]);
     const result = "Hello, World!";
-    expect(LL.$fold(f, result, xs)).toEqual(result);
+    expect(LL.fold(fT, toThunk(result), xs)()).toEqual(result);
   });
   it("should not meet stack overflow", () => {
     const test = () => {
-      const f = (acc: number, x: number) => x + acc;
+      const fT = (acc: Thunk<number>, x: Thunk<number>) => toThunk(x() + acc());
       const length = STACK_OVERFLOW_BOUND;
-      const xs = LL.$range(0, length);
-      return LL.$fold(f, 0, xs);
+      const xs = LL.range(0, length);
+      return LL.fold(fT, toThunk(0), xs);
     };
     expect(test).not.toThrow(RangeError);
   });
 });
 
-describe("$head:", () => {
+describe("head:", () => {
   it("should properly take 1 element and force evaluation", () => {
     const length = 10;
-    const xs = LL.$range(0, length);
+    const xs = LL.range(0, length);
     const result = range(0, length)[0];
-    expect(LL.$head(xs)).toEqual(result);
+    expect(LL.head(xs)()).toEqual(result);
   });
   it("should panic when an empty list is given", () => {
     const test = () => {
       const xs = LL.fromArray([]);
-      return LL.$head(xs);
+      return LL.head(xs);
     };
     expect(test).toThrow(LL.LinkedListError);
     expect(test).toThrow("empty list");
   });
 });
 
-describe("$last:", () => {
+describe("last:", () => {
   it("should properly return a last element and force evaluation", () => {
     const length = 10;
-    const xs = LL.$range(0, length);
+    const xs = LL.range(0, length);
     const result = range(0, length)[length - 1];
-    expect(LL.$last(xs)).toEqual(result);
+    expect(LL.last(xs)).toEqual(result);
   });
   it("should throw LinkedListError when an empty list is given", () => {
     const test = () => {
       const xs = LL.fromArray([]);
-      return LL.$last(xs);
+      return LL.last(xs);
     };
     expect(test).toThrow(LL.LinkedListError);
     expect(test).toThrow("empty list");
@@ -237,8 +197,8 @@ describe("$last:", () => {
   it("should not meet stack overflow", () => {
     const test = () => {
       const length = STACK_OVERFLOW_BOUND;
-      const xs = LL.$range(0, length);
-      return LL.$last(xs);
+      const xs = LL.range(0, length);
+      return LL.last(xs);
     };
     expect(test).not.toThrow(RangeError);
   });
@@ -247,7 +207,7 @@ describe("$last:", () => {
 describe("tail:", () => {
   it("should properly return a list without the first element", () => {
     const length = 10;
-    const xs = LL.tail(LL.$range(0, length));
+    const xs = LL.tail(LL.range(0, length));
     const result = range(0, length).slice(1);
     expect(LL.unsafeToArray(xs)).toEqual(result);
   });
@@ -264,7 +224,7 @@ describe("tail:", () => {
 describe("init:", () => {
   it("should properly return a list without the last element", () => {
     const length = 10;
-    const xs = LL.init(LL.$range(0, length));
+    const xs = LL.init(LL.range(0, length));
     const result = range(0, length).slice(0, length - 1);
     expect(LL.unsafeToArray(xs)).toEqual(result);
   });
@@ -279,27 +239,27 @@ describe("init:", () => {
   it("should not meet stack overflow", () => {
     const test = () => {
       const length = STACK_OVERFLOW_BOUND;
-      const xs = LL.$range(0, length);
+      const xs = LL.range(0, length);
       return LL.init(xs);
     };
     expect(test).not.toThrow(RangeError);
   });
 });
 
-describe("$at:", () => {
+describe("at:", () => {
   it("should properly return an element at the given index", () => {
     const length = 10;
     const idx = 5;
-    const xs = LL.$range(0, length);
+    const xs = LL.range(0, length);
     const result = range(0, length)[idx];
-    expect(LL.$at(xs, idx)).toEqual(result);
+    expect(LL.at(xs, idx)()).toEqual(result);
   });
   it("should throw LinkedListError when a negative index is given", () => {
     const test = () => {
       const length = 10;
       const idx = -1;
-      const xs = LL.$range(0, length);
-      return LL.$at(xs, idx);
+      const xs = LL.range(0, length);
+      return LL.at(xs, idx);
     };
     expect(test).toThrow(LL.LinkedListError);
     expect(test).toThrow("negative index");
@@ -308,7 +268,7 @@ describe("$at:", () => {
     const test = () => {
       const idx = 0;
       const xs = LL.fromArray([]);
-      return LL.$at(xs, idx);
+      return LL.at(xs, idx);
     };
     expect(test).toThrow(LL.LinkedListError);
     expect(test).toThrow("index too large");
@@ -316,59 +276,66 @@ describe("$at:", () => {
   it("should not meet stack overflow", () => {
     const test = () => {
       const length = STACK_OVERFLOW_BOUND;
-      const xs = LL.$range(0, length);
-      return LL.$at(xs, length - 1);
+      const xs = LL.range(0, length);
+      return LL.at(xs, length - 1);
     };
     expect(test).not.toThrow(RangeError);
   });
 });
 
-describe("$prepended:", () => {
+describe("prepended:", () => {
   it("should properly prepend the given value", () => {
     const length = 10;
-    const xs = LL.$range(0, length);
-    const ys = LL.$prepended(-1, xs);
+    const xs = LL.range(0, length);
+    const ys = LL.prepended(toThunk(-1), xs);
     const result = range(0, length);
     result.unshift(-1);
     expect(LL.unsafeToArray(ys)).toEqual(result);
   });
   it("should properly work with an empty list", () => {
     const xs = LL.fromArray([]);
-    const ys = LL.$prepended(-1, xs);
+    const ys = LL.prepended(toThunk(-1), xs);
     const result: number[] = [];
     result.unshift(-1);
     expect(LL.unsafeToArray(ys)).toEqual(result);
   });
   it("should work properly with multiple prepends", () => {
     const length = 10;
-    const xs = LL.$range(0, length);
-    const ys = LL.$prepended(-3, LL.$prepended(-2, LL.$prepended(-1, xs)));
+    const xs = LL.range(0, length);
+    const ys = LL.prepended(
+      toThunk(-3),
+      LL.prepended(toThunk(-2), LL.prepended(toThunk(-1), xs)),
+    );
     const result = range(0, length);
     [-1, -2, -3].forEach((e) => result.unshift(e));
     expect(LL.unsafeToArray(ys)).toEqual(result);
   });
   it("should work properly with different list operations", () => {
     const isOdd = (e: number) => e % 2 === 1;
+    const isOddT = (e: Thunk<number>) => e() % 2 === 1;
     const length = 20;
-    const xs = LL.$prepended(1, LL.$prepended(2, LL.$range(3, length)));
+    const xs = LL.prepended(toThunk(1), LL.prepended(toThunk(2), LL.range(3, length)));
     const result = range(3, length);
     result.unshift(2);
     result.unshift(1);
-    expect(LL.unsafeToArray(LL.$filter(isOdd, xs))).toEqual(result.filter(isOdd));
+    expect(LL.unsafeToArray(LL.filter(isOddT, xs))).toEqual(result.filter(isOdd));
   });
   it("should work properly with an infinite list", () => {
-    const xs = LL.$range(0);
+    const xs = LL.range(0);
     const [i, j] = [-1, -2];
-    const ys = LL.$prepended(j, LL.$prepended(i, xs));
-    const y1 = LL.$head(ys);
-    const y2 = LL.$at(ys, 1);
-    expect(y1).toEqual(j);
-    expect(y2).toEqual(i);
+    const ys = LL.prepended(toThunk(j), LL.prepended(toThunk(i), xs));
+    const y1 = LL.head(ys);
+    const y2 = LL.at(ys, 1);
+    expect(y1()).toEqual(j);
+    expect(y2()).toEqual(i);
   });
   it("should not touch the original list", () => {
     const length = 10;
-    const xs = LL.$range(0, length);
-    const ys = LL.$prepended(-3, LL.$prepended(-2, LL.$prepended(-1, xs)));
+    const xs = LL.range(0, length);
+    const ys = LL.prepended(
+      toThunk(-3),
+      LL.prepended(toThunk(-2), LL.prepended(toThunk(-1), xs)),
+    );
     const result = range(0, length);
     [-1, -2, -3].forEach((e) => result.unshift(e));
     expect(LL.unsafeToArray(xs)).toEqual(range(0, length));
@@ -376,43 +343,44 @@ describe("$prepended:", () => {
   });
 });
 
-describe("$pushed:", () => {
+describe("pushed:", () => {
   it("should properly push the given value", () => {
     const length = 10;
-    const xs = LL.$range(0, length);
-    const ys = LL.$pushed(-1, xs);
+    const xs = LL.range(0, length);
+    const ys = LL.pushed(toThunk(-1), xs);
     const result = range(0, length);
     result.push(-1);
     expect(LL.unsafeToArray(ys)).toEqual(result);
   });
   it("should properly work with an empty list", () => {
     const xs = LL.fromArray([]);
-    const ys = LL.$pushed(-1, xs);
+    const ys = LL.pushed(toThunk(-1), xs);
     const result: number[] = [];
     result.unshift(-1);
     expect(LL.unsafeToArray(ys)).toEqual(result);
   });
   it("should work properly with multiple pushes", () => {
     const length = 10;
-    const xs = LL.$range(0, length);
-    const ys = LL.$pushed(-3, LL.$pushed(-2, LL.$pushed(-1, xs)));
+    const xs = LL.range(0, length);
+    const ys = LL.pushed(toThunk(-3), LL.pushed(toThunk(-2), LL.pushed(toThunk(-1), xs)));
     const result = range(0, length);
     [-1, -2, -3].forEach((e) => result.push(e));
     expect(LL.unsafeToArray(ys)).toEqual(result);
   });
   it("should work properly with different list operations", () => {
     const isOdd = (e: number) => e % 2 === 1;
+    const isOddT = (e: Thunk<number>) => e() % 2 === 1;
     const length = 20;
-    const xs = LL.$pushed(21, LL.$pushed(20, LL.$range(3, length)));
+    const xs = LL.pushed(toThunk(21), LL.pushed(toThunk(20), LL.range(3, length)));
     const result = range(3, length);
     result.push(20);
     result.push(21);
-    expect(LL.unsafeToArray(LL.$filter(isOdd, xs))).toEqual(result.filter(isOdd));
+    expect(LL.unsafeToArray(LL.filter(isOddT, xs))).toEqual(result.filter(isOdd));
   });
   it("should not touch the original list", () => {
     const length = 10;
-    const xs = LL.$range(0, length);
-    const ys = LL.$pushed(-3, LL.$pushed(-2, LL.$pushed(-1, xs)));
+    const xs = LL.range(0, length);
+    const ys = LL.pushed(toThunk(-3), LL.pushed(toThunk(-2), LL.pushed(toThunk(-1), xs)));
     const result = range(0, length);
     [-1, -2, -3].forEach((e) => result.push(e));
     expect(LL.unsafeToArray(xs)).toEqual(range(0, length));
